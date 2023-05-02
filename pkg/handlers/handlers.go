f951e68e0cd417fb819c323b6faf6e51c8d7d98c
Amir <amirarifdzanov@gmail.com>
2023-05-02 17:48:56 +0500

    new file Weather

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,11 +1,7 @@
 package handlers
 
 import (
-	"OpenWeatherMap-API/pkg/models"
-	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
-	"log"
-	"net/http"
 )
 
 type handler struct {
@@ -14,19 +10,4 @@ type handler struct {
 
 func NewHandler(db *gorm.DB) handler {
 	return handler{DB: db}
-}
-func (h *handler) GetOneWeather(c *gin.Context) {
-	//Task1
-	// Use the Open Weather API to receive weather data for the given location
-	// Используйте Open Weather API для получения данных о погоде для заданного местоположения.
-	var city models.City
-	result := h.DB.Find(&city)
-	if result.Error != nil {
-		log.Println("DB error - cannot find City Weather:", result.Error)
-		c.JSON(http.StatusNotFound, gin.H{
-			"Error": "Internal Server Error",
-		})
-		return
-	}
-	c.JSON(http.StatusOK, city)
 }
