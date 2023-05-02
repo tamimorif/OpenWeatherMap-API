9afb4a1596ca2e9840b8838f72f2899c9894f480
Tamim <tamimarif03@gmail.com>
2023-05-02 17:33:30 +0500

    make GetOneWeather

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"OpenWeatherMap-API/pkg/models"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"log"
+	"net/http"
 )
 
 type Handler struct {
@@ -12,20 +15,20 @@ type Handler struct {
 func NewHandler(db *gorm.DB) Handler {
 	return Handler{DB: db}
 }
-func (h *Handler) GetWeather(c *gin.Context) {
+func (h *Handler) GetOneWeather(c *gin.Context) {
 	//Task1
 	// Use the Open Weather API to receive weather data for the given location
-	//var cities []models.City
-	//result := h.DB.Find(&cities)
-	//if result.Error != nil {
-	//	log.Println("DB error - cannot find tasks:", result.Error)
-	//	c.JSON(http.StatusInternalServerError, gin.H{
-	//		"message": "Internal Server Error",
-	//	})
-	//	return
-	//}
-	//
-	//c.JSON(http.StatusOK, cities)
+	// Используйте Open Weather API для получения данных о погоде для заданного местоположения.
+	var city models.City
+	result := h.DB.Find(&city)
+	if result.Error != nil {
+		log.Println("DB error - cannot find City Weather:", result.Error)
+		c.JSON(http.StatusNotFound, gin.H{
+			"Error": "Internal Server Error",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, city)
 	//Task2
 	// Write the weather data to the response
 }
